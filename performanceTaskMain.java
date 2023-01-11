823033b19e56b76beaa69cd8c1760fa676f0e889
Albert Tieu <albert.tieu@mrjaffesclass.com>
2023-01-11 13:42:17 -0800

    started work

--- performanceTaskMain.java
+++ performanceTaskMain.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 /**
  * Write a description of class performanceTaskMain here.
@@ -7,27 +8,51 @@
  */
 public class performanceTaskMain
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class performanceTaskMain
-     */
-    public performanceTaskMain()
-    {
-        // initialise instance variables
-        x = 0;
+    Scanner scanner = new Scanner(System.in);
+    String input = "";
+    int LOWER_BOUND = 8;
+    int UPPER_BOUND = 16;
+    int simSize;
+    boolean shouldContinue = true;
+    boolean activeSim = false;
+    int [][] sim;
+    public performanceTaskMain() {
+        // :D
     }
-
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+    
+    public void runEventLoop() {
+        while (shouldContinue) {
+            if (activeSim == false) {
+                System.out.println("Hi!");
+            } else {
+                
+            }
+            System.out.println("[C] - close program");
+            System.out.println("[N] - new simulation");
+            input = scanner.next();
+            if (input.equals("C")) {
+                shouldContinue = false;
+            }
+            if (input.equals("N")) {
+                sim = createSimulation(simSize);
+                activeSim = true;
+            }
+        }
     }
+    
+    public static void main (String[] args) {
+        performanceTaskMain conwayGame = new performanceTaskMain();
+        conwayGame.runEventLoop();
+    }
+    
+    public int[][] createSimulation(int simSize) {
+        int[][] array2d = new int[simSize][simSize]; 
+        for (int a = 0;a < simSize;a++) {
+            for (int i = 0;i < simSize;i++) {
+                array2d[a][i] = 0;
+            }
+        }
+        return array2d;
+    }
+    
 }
