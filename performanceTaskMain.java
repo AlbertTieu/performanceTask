264f59df0223ec4e7bf1b9468324976325013bf2
Albert Tieu <albert.tieu@mrjaffesclass.com>
2023-04-25 14:22:12 -0700

    need to fix the method code up. what a mess

--- performanceTaskMain.java
+++ performanceTaskMain.java
@@ -1,5 +1,7 @@
 import java.util.Scanner;
 import java.util.InputMismatchException;
+import java.util.List;
+import java.util.ArrayList;
 
 public class performanceTaskMain {
     Scanner scanner = new Scanner(System.in);
@@ -9,6 +11,8 @@ public class performanceTaskMain {
     int UPPER_BOUND = 35;
     int time = 1000;
     int genNum = 0;
+    int w;
+    int h;
     boolean shouldContinue = true;
     boolean activeSim = false;
     boolean errorSimSize = false;
@@ -16,6 +20,7 @@ public class performanceTaskMain {
     boolean successMsg = false;
     int [][] sim;
     int [][] lastGen;
+    List <int[][]> genHist = new ArrayList <int[][]> (10);
     public performanceTaskMain() {
         // :D
     }
@@ -25,7 +30,7 @@ public class performanceTaskMain {
             System.out.print('\u000C');
             input = "";
             if (errorAuto == true) {
-                System.out.println("error: please enter integer");
+                System.out.println("error: please enter a valid integer");
                 errorAuto = false;
             }
             if (errorSimSize == true) {
@@ -82,9 +87,25 @@ public class performanceTaskMain {
                 genNum = 0;
                 scanner.nextLine();
             }
+            if (input.equals("M")) {
+                System.out.println("enter width:");
+                try {
+                    w = scanner.nextInt();
+                } catch (InputMismatchException error) {
+                    errorAuto = true;
+                }
+                if (errorAuto != true) {
+                    System.out.println("enter length:");
+                    try {
+                        h = scanner.nextInt();
+                        createCustomSimulation(h,w);
+                    } catch (InputMismatchException error) {
+                        errorAuto = true;
+                    }
+                }
+            }
             
             if (input.equals("Z") && activeSim == true) {
-                
                 sim = simNextGen();
                 input = "";
                 genNum++;
@@ -93,8 +114,10 @@ public class performanceTaskMain {
                 input = "";
                 scanner.nextLine();
             }
-            if (input.equals("U") && activeSim == true && genNum > 0) {
-                
+            if (input.equals("X") && activeSim == true && genNum > 0) {
+                sim = genHist.get(genHist.size() - 1);
+                genHist.remove(genHist.size()-1);
+                genNum = genNum - 1;
             }
             
             if (input.equals ("A") && activeSim == true) {
@@ -136,24 +159,28 @@ public class performanceTaskMain {
         conwayGame.runEventLoop();
     }
     
-    public void createSimulation(int simulation_size) {
-        //simSize = simulation_size;
-        //int[][] array2d = new int[simSize+2][simSize+2]; 
-        //int status;
-        //for (int a = 0;a < simSize;a++) {
-        //    for (int i = 0;i < simSize;i++) {
-        //        if (i == 0 || a == 0 || i == simSize -1 || a == simSize -1) {
-        //            array2d[i][a] = 0;
-        //        } else {
-        //            status = (int)(Math.random() * 2);
-        //            array2d[i][a] = status;
-        //        }
-        //    }
-        //}
-        //return array2d;
+    public void createCustomSimulation(int height, int width) {
+        if (width < 0 || height < 0) {
+            errorAuto = true;
+        } else {
+           int[][] array2d = new int[height+2][width+2];
+           int status;
+           for (int a = 0;a < simSize;a++) {
+               for (int i = 0;i < simSize;i++) {
+                    if (i == 0 || a == 0 || i == simSize -1 || a == simSize -1) {
+                       array2d[i][a] = 0;
+                   } else {
+                       status = (int)(Math.random() * 2);
+                       array2d[i][a] = status;
+                   }
+               }
+           }
+           sim = array2d;
+        }
     }
     
     public int[][] simNextGen() {
+        genHist.add(sim);
         int[][] array2d = new int[simSize + 2][simSize +2];
         int status;
         int adjLiving;
