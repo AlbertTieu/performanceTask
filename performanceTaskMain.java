3f1a78472f108195dbf2a46ce2d7ac20ba3745c9
Albert Tieu <albert.tieu@mrjaffesclass.com>
2023-04-26 14:21:05 -0700

    hopefully done

--- performanceTaskMain.java
+++ performanceTaskMain.java
@@ -16,8 +16,9 @@ public class performanceTaskMain {
     boolean shouldContinue = true;
     boolean activeSim = false;
     boolean errorSimSize = false;
-    boolean errorAuto = false;
+    boolean errorInt = false;
     boolean successMsg = false;
+    boolean activeCusSim = false;
     int [][] sim;
     int [][] lastGen;
     List <int[][]> genHist = new ArrayList <int[][]> (10);
@@ -29,16 +30,16 @@ public class performanceTaskMain {
         while (shouldContinue) {
             System.out.print('\u000C');
             input = "";
-            if (errorAuto == true) {
-                System.out.println("error: please enter a valid integer");
-                errorAuto = false;
+            if (errorInt == true) {
+                System.out.println("error: please enter valid integer");
+                errorInt = false;
             }
             if (errorSimSize == true) {
                 System.out.println("error: please enter an integer between " + LOWER_BOUND + " - " + UPPER_BOUND);
                 errorSimSize = false;
             }
             
-            if (activeSim == false) {
+            if (activeSim == false && activeCusSim == false) {
                 System.out.println("Hi!");
             } else {
                 printSim();
@@ -47,6 +48,7 @@ public class performanceTaskMain {
                 System.out.println("[A] - autoplay simulation");
             }
             System.out.println("[N] - new simulation");
+            System.out.println("[M] - new custom sim");
             System.out.println("[C] - close program");
             input = scanner.next();
             
@@ -79,6 +81,7 @@ public class performanceTaskMain {
                         }
                         sim = array2d;
                         activeSim = true;
+                        activeCusSim = false;
                     }
                 } catch (InputMismatchException error) {
                     errorSimSize = true;
@@ -92,20 +95,20 @@ public class performanceTaskMain {
                 try {
                     w = scanner.nextInt();
                 } catch (InputMismatchException error) {
-                    errorAuto = true;
+                    errorInt = true;
                 }
-                if (errorAuto != true) {
+                if (errorInt != true) {
                     System.out.println("enter length:");
                     try {
                         h = scanner.nextInt();
                         createCustomSimulation(h,w);
                     } catch (InputMismatchException error) {
-                        errorAuto = true;
+                        errorInt = true;
                     }
                 }
             }
             
-            if (input.equals("Z") && activeSim == true) {
+            if (input.equals("Z") && (activeSim == true || activeCusSim == true)) {
                 sim = simNextGen();
                 input = "";
                 genNum++;
@@ -114,21 +117,21 @@ public class performanceTaskMain {
                 input = "";
                 scanner.nextLine();
             }
-            if (input.equals("X") && activeSim == true && genNum > 0) {
+            if (input.equals("X") && (activeSim == true || activeCusSim == true) && genNum > 0) {
                 sim = genHist.get(genHist.size() - 1);
                 genHist.remove(genHist.size()-1);
                 genNum = genNum - 1;
             }
             
-            if (input.equals ("A") && activeSim == true) {
+            if (input.equals ("A") && (activeSim == true || activeCusSim == true)) {
                 System.out.println("define an interval (ms)");
                 try {
                     time = scanner.nextInt();
                 } catch (InputMismatchException error) {
-                    errorAuto = true;
+                    errorInt = true;
                 }
                 System.out.println("how many generations?");
-                if (errorAuto != true) {
+                if (errorInt != true) {
                     int gens;
                     try {
                         gens = scanner.nextInt();
@@ -144,11 +147,11 @@ public class performanceTaskMain {
                             }
                         }
                     } catch (InputMismatchException error) {
-                        errorAuto = true;
+                        errorInt = true;
                         scanner.nextLine();
                     }
                 }
-            } else if (input.equals ("A") && activeSim == false) {
+            } else if (input.equals ("A") && (activeSim == false || activeCusSim == false)) {
                 scanner.nextLine();
             }
         }
@@ -161,27 +164,32 @@ public class performanceTaskMain {
     
     public void createCustomSimulation(int height, int width) {
         if (width < 0 || height < 0) {
-            errorAuto = true;
+            errorInt = true;
         } else {
            int[][] array2d = new int[height+2][width+2];
            int status;
-           for (int a = 0;a < simSize;a++) {
-               for (int i = 0;i < simSize;i++) {
-                    if (i == 0 || a == 0 || i == simSize -1 || a == simSize -1) {
+           for (int a = 0;a < width+2;a++) {
+               for (int i = 0;i < height+2;i++) {
+                    if (i == 0 || a == 0 || i == height +1 || a == width +1) {
                        array2d[i][a] = 0;
                    } else {
                        status = (int)(Math.random() * 2);
                        array2d[i][a] = status;
                    }
+                   
                }
            }
            sim = array2d;
+           activeCusSim = true;
+           activeSim = false;
         }
     }
     
     public int[][] simNextGen() {
         genHist.add(sim);
-        int[][] array2d = new int[simSize + 2][simSize +2];
+        int array2d[][]=new int [999][999];
+        if (activeSim == true) {
+        int[][] arrayN2d = new int[simSize + 2][simSize +2];
         int status;
         int adjLiving;
         for (int a = 1;a < simSize +1;a++) {
@@ -189,20 +197,43 @@ public class performanceTaskMain {
                 adjLiving = sim[i-1][a-1] + sim[i-1][a] + sim[i-1][a+1] + sim[i][a+1] + sim[i][a-1] + sim[i+1][a-1] + sim[i+1][a] + sim[i+1][a+1];
                 if (sim[i][a] == 0) {
                     if (adjLiving == 3) {
-                        array2d[i][a] = 1;
+                        arrayN2d[i][a] = 1;
                     }
                 } else if (adjLiving == 2 || adjLiving == 3) {
-                    array2d[i][a] = 1;
+                    arrayN2d[i][a] = 1;
                 } else {
-                    array2d[i][a] = 0;
+                    arrayN2d[i][a] = 0;
                 }
             }
         }
-        return array2d;
+        array2d = arrayN2d;
+    }
+    if (activeCusSim == true) {
+        int[][] arrayM2d = new int[h + 2][w +2];
+        int status;
+        int adjLiving;
+        for (int a = 1;a < w +1;a++) {
+            for (int i = 1;i < h +1;i++) {
+                adjLiving = sim[i-1][a-1] + sim[i-1][a] + sim[i-1][a+1] + sim[i][a+1] + sim[i][a-1] + sim[i+1][a-1] + sim[i+1][a] + sim[i+1][a+1];
+                if (sim[i][a] == 0) {
+                    if (adjLiving == 3) {
+                        arrayM2d[i][a] = 1;
+                    }
+                } else if (adjLiving == 2 || adjLiving == 3) {
+                    arrayM2d[i][a] = 1;
+                } else {
+                    arrayM2d[i][a] = 0;
+                }
+            }
+        }
+        array2d = arrayM2d;
+    }
+    return array2d;
     }
     
     public void printSim() {
         //this is the method for printing the conway simulation game itself
+        if (activeSim == true) {
         for (int a = 0;a < simSize +2;a++) {
             for (int i = 0;i < simSize+2;i++) {
                 if (sim[i][a] == 0) {
@@ -216,5 +247,21 @@ public class performanceTaskMain {
         }
         System.out.print("generation " +genNum);
         System.out.println();
+        }
+        if (activeCusSim == true) {
+            for (int a = 0;a < w +2;a++) {
+            for (int i = 0;i < h+2;i++) {
+                if (sim[i][a] == 0) {
+                    System.out.print(" ");
+                } else {
+                    System.out.print("X");
+                }
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+        System.out.print("generation " +genNum);
+        System.out.println();
+        }
     }
 }
